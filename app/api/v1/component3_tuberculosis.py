f2f61cd8a22e5cebbb536ef503d2c30096a6075c
Chanuka Nanayakkara <155652752+chanukananayakkara@users.noreply.github.com>
2026-05-10 18:14:17 +0530

    Completed the MongoDB connection

--- app/api/v1/component3_tuberculosis.py
+++ app/api/v1/component3_tuberculosis.py
@@ -1,11 +1,14 @@
-from fastapi import APIRouter, UploadFile, File, HTTPException
+from fastapi import APIRouter, UploadFile, File, Form, HTTPException
 from app.services.comp3_service import process_tb_scan
 from app.models.component3_schema import TBPredictionResponse
 
 router = APIRouter(prefix="/component3", tags=["Tuberculosis (GhostNet+MobileViT)"])
 
 @router.post("/predict", response_model=TBPredictionResponse)
-async def analyze_tb_scan(file: UploadFile = File(...)):
+async def analyze_tb_scan(
+    patient_id: str = Form(...),
+    file: UploadFile = File(...)
+):
     """
     Upload a Chest X-Ray image to predict the presence of Tuberculosis.
     """
@@ -14,5 +17,5 @@ async def analyze_tb_scan(file: UploadFile = File(...)):
         raise HTTPException(status_code=400, detail="Invalid file type. Please upload an image.")
     
     # Pass to the service layer
-    result = await process_tb_scan(file)
+    result = await process_tb_scan(patient_id, file)
     return result
