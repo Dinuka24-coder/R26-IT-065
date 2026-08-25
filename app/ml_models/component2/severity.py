35c20ce3f174fc28d8e11bc2652be80b926b7732
Dinuka Amarasekara <it22233226@my.sliit.lk>
2026-08-25 16:24:47 +0530

    Updated the pneumonia chance to detect more sensitive xrays

--- app/ml_models/component2/severity.py
+++ app/ml_models/component2/severity.py
@@ -3,9 +3,9 @@ import numpy as np
 
 def calculate_pneumonia_severity(confidence_score):
     """Calculates a qualitative severity level based on the AI confidence score."""
-    if confidence_score < 50:
+    if confidence_score < 47:
         return "N/A (Normal)"
-    elif 50 <= confidence_score < 70:
+    elif 47 <= confidence_score < 70:
         return "Mild / Early Stage"
     elif 70 <= confidence_score < 90:
         return "Moderate"
